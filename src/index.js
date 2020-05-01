bce1647c4bae209e6371c07afc48d533b0913be8
bloodysysy73 <sylvain.guehria@gmail.com>
2020-05-01 16:24:28 +0200

    Revert "test 4"
    This reverts commit dcd94f505a6ad4c036f2c4980ef86a681b3f9233.
    
     Modifications qui seront validées :

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import reduxThunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import reducers from "./reducers";
+import history from "./history";
+import AdminLayout from "layouts/Admin.jsx";
+// import UtilisateurDelete from "components/Utilisateur/UtilisateurDelete";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.1.0";
+import "assets/demo/demo.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
 import App from './App';
+
+
+
+
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  (applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>

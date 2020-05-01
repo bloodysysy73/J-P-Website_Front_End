26ed5ebfce125c3c1f06cd78b058b1a5b8dcd01c
bloodysysy73 <sylvain.guehria@gmail.com>
2020-05-01 16:25:05 +0200

    Revert "test 2"
    This reverts commit b7cfb4d88746ee490a73cc0b3ae690ed7247bca1.
    
     Modifications qui seront validées :

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO WORLD
+          <h4>HELLO WORLD</h4>
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -19,10 +19,10 @@ import App from './App';
 
 
 
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
-  (applyMiddleware(reduxThunk))
+  composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(

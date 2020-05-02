887ec53706d1ebe0708fb54ea714a9b0bb8cc093
bloodysysy73 <sylvain.guehria@gmail.com>
2020-05-02 11:35:51 +0200

    modif https

--- src/variables/general.js
+++ src/variables/general.js
@@ -1,1 +1,1 @@
-export const ElasticBeanStalk = "http://jumeauxetplusapp-env.eba-3umrdymy.eu-west-3.elasticbeanstalk.com"
+export const ElasticBeanStalk = "https://jumeauxetplusapp-env.eba-3umrdymy.eu-west-3.elasticbeanstalk.com"
